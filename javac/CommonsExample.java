e5cbc8302106b87282eb6655169c4f3d18fdf8c9
Lukasz Janicki <lukasz.janicki@gft.com>
2019-09-24 18:29:25 +0100

    CommonsExample start

---
+++ javac/CommonsExample.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CommonsExample {
+
+    public static void main( String[] args ) {
+
+        Scanner in = new Scanner( System.in );
+        System.out.print( "Wpisz wartosc: " );
+        String valueFromUser = in.nextLine();
+
+        String changedValue = swapCase( valueFromUser );
+
+        System.out.println( "Wartosc zmieniona: " + changedValue );
+
+    }
+
+    private static String swapCase( String initialValue ) {
+        return initialValue;
+    }
+
+}
